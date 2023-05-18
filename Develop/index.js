f3a415e93da5cd4cfdd6aa72ea127a561392d2fb
mrbll48 <william.marks87@gmail.com>
2023-05-18 14:17:51 -0700

    readme functioning

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateReadMe = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "projTitle",
+  },
+  {
+    type: "input",
+    message: "Write your project description here.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do you install your application?",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "How do you use your application?",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "What license does your application use?",
+    name: "license",
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      'BSD 2-Clause "Simplified" License',
+      'BSD 3-Clause "New" or "Revised" License',
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public Licesne v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+      "No License",
+    ],
+  },
+  {
+    type: "input",
+    message: "Who helped you create this application?",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "What tests were conducted on this application?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "How can you be reached for further questions?",
+    name: "furtherQuestions",
+  },
+  {
+    type: "input",
+    message: "What is your github URL?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt(questions).then((answers) => {
+  const readMeContent = generateReadMe(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+  fs.writeFile("readme.md", readMeContent, (err) =>
+    err ? console.log(err) : console.log("successfully created readme.md")
+  );
+});
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,111 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache License 2.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-green)`;
+    case "GNU General Public License v3.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-red)`;
+    case "MIT License":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-blue)`;
+    case "BSD 2-Clause 'Simplified' License":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-yellow)`;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-FB02FF)`;
+    case "Boost Software License 1.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-1D001E)`;
+    case "Creative Commons Zero v1.0 Universal":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-03C5E4)`;
+    case "Eclipse Public License 2.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-086A02)`;
+    case "GNU Affero General Public License v3.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-6A1002)`;
+    case "GNU General Public License v2.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-000000)`;
+    case "GNU Lesser General Public Licesne v2.1":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-BABABA)`;
+    case "Mozilla Public License 2.0":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-043676)`;
+    case "The Unlicense":
+      return `![Project License](https://img.shields.io/badge/License-${encodeURIComponent(
+        license
+      )}-DA72A0)`;
+    case "No License":
+      return "";
+  }
 }
 
-module.exports = generateMarkdown;
+function renderLicenseLink(license) {
+  return `[${renderLicenseBadge(license)}](https://choosealicense.com)`;
+}
+
+const generateReadMe = ({
+  projTitle,
+  description,
+  install,
+  usage,
+  license,
+  contribute,
+  tests,
+  furtherQuestions,
+  github,
+  email,
+}) =>
+  `# ${projTitle}
+${renderLicenseLink(license)}
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description 
+${description}
+
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## License
+${license}
+
+## Contributing
+${contribute}
+
+## Tests
+${tests}
+
+## Questions
+${furtherQuestions}
+${github}
+${email}`;
+
+module.exports = generateReadMe;
